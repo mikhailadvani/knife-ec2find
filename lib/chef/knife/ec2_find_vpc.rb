d419315fc74ec14a3042e4c71c64b7bd1dfab680
mikhail <mikhaila@thoughworks.com>
2015-11-25 21:29:40 +0530

    Added search VPC capability

---
+++ lib/chef/knife/ec2_find_vpc.rb
@@ -0,0 +1,44 @@
+require_relative 'ec2_find_base'
+module EC2Find
+  class Ec2findVpc < Ec2findBase
+
+    banner "knife ec2find vpc TAGS"
+
+    option :tags,
+           :short => "-T T=V[,T=V[,T=V...]]",
+           :long => "--tags Tag=Value[,Tag=Value[,Tag=Value...]]",
+           :description => "Lists instances created with set of tags"
+
+    option :aws_access_key_id,
+           :long => "--aws-access-key-id KEY",
+           :description => "Your AWS Access Key ID"
+
+    option :aws_secret_access_key,
+           :short => "-K SECRET",
+           :long => "--aws-secret-access-key",
+           :description => "Your AWS API Secret Access Key"
+
+    option :projection,
+           :short => "-a ATTRIBUTES",
+           :long => "--attributes",
+           :description => "To project only required attributes"
+
+    option :suppress_attribute_names,
+           :long => "--suppress-attribute-names",
+           :description => "Print only entity description attribute values and no keys",
+           :boolean => true,
+           :default => false
+
+
+    private
+    def default_attributes
+      ["vpc_id", "cidr_block", "instance_tenancy", "is_default", "tags"]
+    end
+
+    def findby tags
+      ec2connect
+      @ec2client.describe_vpcs({dry_run: false, filters: tags}).vpcs
+    end
+
+  end
+end
